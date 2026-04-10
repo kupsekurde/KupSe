979578aca0a7a6a35b4c8b030c213eb581bceaa3
kupsekurde <soko839soko@gmail.com>
2026-04-10 13:54:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const URL_S = 'https://zeymooitrdcbgrrpzhed.supabase.co';
-const KEY_S = 'sb_publishable_tTUBju7up_8DW05IAK4qHQ_bqkns4JO';
+const KEY_S = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpleW1vb2l0cmRjYmdycnB6aGVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MDA4MzgsImV4cCI6MjA5MTM3NjgzOH0.dwTF_sCtvkcN5v6fb2vHoThplzgc42ZY-pVx2LySkYo';
 
 // Sprawdzamy czy biblioteka się załadowała
 if (!window.supabase) {
