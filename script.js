6c137f7b0b73084fecace3e3df96e2a8427bebf5
kupsekurde <soko839soko@gmail.com>
2026-06-12 22:20:04 +0200

    Clean up script.js by removing unnecessary lines
    Removed unused content and comments from script.js.

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-1 selected
-
-Skip to content
-Using Gmail with screen readers
-Enable desktop notifications for Gmail.
-   OK  No thanks
-Conversations
-0% of 15 GB used
-Terms · Privacy · Program Policies
-Last account activity: 0 minutes ago
-Currently being used in 3 other locations · Details
 window.updateCounter = (id, countId, max) => {
     const el = document.getElementById(id);
     if (!el) return;
